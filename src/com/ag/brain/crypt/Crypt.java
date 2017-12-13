2482ed2e3e5d6c873e8d70993b04b4873c19230d
Alessandro Guaresti <alessguar@gmail.com>
2017-12-12 16:42:19 -0800

    more work into both the encryption and image creation process.

--- src/com/ag/brain/crypt/Crypt.java
+++ src/com/ag/brain/crypt/Crypt.java
@@ -1,11 +1,66 @@
 package com.ag.brain.crypt;
 
-import javafx.scene.image.Image;
+import javafx.scene.image.*;
+import javafx.scene.paint.Color;
+
+/*
+*			 W
+*	___________________	
+*	******************* 
+*	*                 *	|
+*	*                 *	|
+*	*                 *	|L
+*	*                 *	|
+*	*                 *	|
+*	******************* 
+**/
 
 public abstract class Crypt{
 
-	private Image image;
-	private ImageFactory iF;
+	private String inMess
+	private Image inImg;
+	private ImageFactory iF;// might be temporary 
+	private static PixelWriter pW;
+	private static PixelReader pR;
+	private Double l;
+	private Double w;
+	private Color[][] pic;
+
+	public Crypt(String in){
+		inMess = in;
+		calcBounds();
+	}
+
+	public Crypt(Image i){
+		inImg = (WritableImage) i;
+		inMess = "";
+		calcBounds();
+	}
+
+	public void calcBounds(){
+		// message
+		if(inMess.isEmpty()){
+			//4:3 aspect ratio
+			int len = inMess.length();
+			int numOfColors = len / 3;
+			int numOfUnpaired = len % 3;
+			w = new Double(Math.floor((numOfColors+((numOfUnpaired%3 != 0)? 1:0))/(4/3.0))+1);
+			l = new Double(Math.floor((4/3.0) * w)+1);
+			pic = new Color[l.intValue()][w.intValue()];
+		}else if(){ // image
+			w = inImg.getWidth();
+			h = inImg.getHeight();
+
+		}
+	}
+
+	public static void setAllBlank(){
+		for (int row = 0;row < pic.length; row++) {
+			for (int col = 0;col < pic[row].length; col++) {
+				pic[row][col] = Color.web("FFFFFF");
+			}
+		}
+	}
 
 
 
--- src/com/ag/main/TestMain.java
+++ src/com/ag/main/TestMain.java
@@ -1,16 +1,18 @@
 package com.ag.main;
 
-import javafx.application.Application;
+import java.util.Scanner;
+
+/*import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
+import javafx.stage.Stage;*/
 
-public class TestMain extends Application{
+public class TestMain /*extends Application*/{
 
-	private int[][] arrayValues;
+    /*private int[][] arrayValues;
 	private GridPane g;
 	private Integer val;
 	private Integer nVal;
@@ -40,9 +42,43 @@ public class TestMain extends Application{
 
 	public void shift(){
 		nVal = new Integer(val<<3);
-	}
+	}*/
+
+	/*public void aspectTesting(){
+		Scanner s = new Scanner(System.in);
+		boolean f = true;
+		while(f){
+			int in = s.nextInt();
+			if(in == -1){
+				f = false;
+			}else{
+				int completeColors = in/3;
+				int unPairedColors = in%3;
+				double area = Math.floor(in/3.0);
+				// for a 1:1 aspect ratio
+				double lw = Math.floor(Math.sqrt(area));
+				double emptyPix11 = ((lw+1) * (lw+1)) - (completeColors+1);
+				// for a 4:3 aspect ratio
+				double w = Math.floor(Math.sqrt(area / (4/3.0)))+1;
+				double l = Math.floor((4/3.0) * w)+1;
+				double emptyPix43 = (l * w) - (completeColors+1);
+				System.out.println("There would be " + completeColors + " Complete Colors and " + unPairedColors + " unpaired Colors in " + in + " Characters.");
+				System.out.println("For a 1:1 Aspect Ratio the length and width would be " + lw + " pixels.\nWe would have to create " + emptyPix11 + " empty pixels.");
+				System.out.println("For a 4:3 Aspect Ratio the width would be " + l + " pixels and the height would be " + w + "pixels.\nWe would have to create " +emptyPix43 + " empty pixels.");
+			}
+		}
+		s.close();
+	}*/
+
+	private static String s;
 
 	public static void main(String [] args){
-		launch(args);
+		//launch(args);
+		s = null;
+		// System.out.println(s.length());
+		System.out.println(s.isEmpty());
+		s = "";
+		System.out.println(s.length());
+		System.out.println(s.isEmpty());
 	}
 }
