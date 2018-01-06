a09c9edfd8b969b47ff2045498097538492d34be
Alessandro Guaresti <alessguar@gmail.com>
2018-01-05 16:26:26 -0800

    Still working on #28. Started to try to fix the skipping issue. Want to see if i can add spaces to the mix as well.

--- src/com/ag/main/TestMain.java
+++ src/com/ag/main/TestMain.java
@@ -4,6 +4,7 @@ import com.ag.brain.crypt.Crypt;
 import com.ag.brain.crypt.Encrypt;
 import javafx.scene.paint.Color;
 
+import java.util.HashMap;
 import java.util.Random;
 
 public class TestMain /*extends Application*/{
@@ -99,44 +100,91 @@ public class TestMain /*extends Application*/{
 		String[] test1 = TEST_STRING.split(" ");
 		pic[0][0] = Color.rgb(meta[0], meta[1], meta[2], .5);
 		pic[0][1] = Color.rgb(meta[3], meta[4], meta[5], .5);
-		int row = 0, col = 2, i = 0;
+		int row = 0, col = 2, i = 0, cap = 0;
+		boolean flag = true; // true is color & false is color2
 		int[] color = new int[3];
+		int[] color2 = new int[3];
 		for (String a : test1) {
 			char[] t = a.toCharArray();
-			for (char b : t) {
-				int hashVal = Character.hashCode(b);
-				if(i==3){
-					if(row != r.intValue()-1){
-						if (col != c.intValue()-1) {
-							// general cells - the inbetween
-							pic[row][col] = Color.rgb(color[0],color[1],color[2]);
-							col++;
-						}else{
-							//if it is the last column in the row
-							pic[row][col] = Color.rgb(color[0],color[1],color[2]);
-							col = 0;
-							row++;
-						}
-					}else{
-						//if it is the last row
-						if (col != c.intValue()-1) {
-							//all cells before the last cell
-							pic[row][col] = Color.rgb(color[0],color[1],color[2]);
-							col++;
-						}else{
-							//the last cell
-							pic[row][col] = Color.rgb(color[0],color[1],color[2]);
-						}
-					}
-					for(int d: color){
-						d = 0;
-					}
-					i = 0;
-				}else{
-					//initializes the array of hash values to put into colors
-					color[i] = hashVal;
-					i++;
-				}
+            if(flag){
+                if(cap==3){
+                    if(row != r.intValue()-1){
+                        if (col != c.intValue()-1) {
+                            // general cells - the inbetween
+                            pic[row][col] = Color.rgb(color[0],color[1],color[2]);
+                            col++;
+                        }else{
+                            //if it is the last column in the row
+                            pic[row][col] = Color.rgb(color[0],color[1],color[2]);
+                            col = 0;
+                            row++;
+                        }
+                    }else{
+                        //if it is the last row
+                        if (col != c.intValue()-1) {
+                            //all cells before the last cell
+                            pic[row][col] = Color.rgb(color[0],color[1],color[2]);
+                            col++;
+                        }else{
+                            //the last cell
+                            pic[row][col] = Color.rgb(color[0],color[1],color[2]);
+                        }
+                    }
+                    for(int l = 0; l < color.length; l++){
+                        color[l] = 0;
+                    }
+                }
+                cap = 0;
+            }else{
+                if(i==3){
+                    if(row != r.intValue()-1){
+                        if (col != c.intValue()-1) {
+                            // general cells - the inbetween
+                            pic[row][col] = Color.rgb(color2[0],color2[1],color2[2]);
+                            col++;
+                        }else{
+                            //if it is the last column in the row
+                            pic[row][col] = Color.rgb(color2[0],color2[1],color2[2]);
+                            col = 0;
+                            row++;
+                        }
+                    }else{
+                        //if it is the last row
+                        if (col != c.intValue()-1) {
+                            //all cells before the last cell
+                            pic[row][col] = Color.rgb(color2[0],color2[1],color2[2]);
+                            col++;
+                        }else{
+                            //the last cell
+                            pic[row][col] = Color.rgb(color2[0],color2[1],color2[2]);
+                        }
+                    }
+                    for(int l = 0; l < color2.length; l++){
+                        color2[l] = 0;
+                    }
+                }
+                i = 0;
+            }
+            for (char b : t) {
+                //initializes the array of hash values to put into colors
+                int hashVal = Character.hashCode(b);
+				/*try putting a secondary array to hold the rest of the values
+				and have a flag system to switch which one it goes into*/
+                if(flag) {
+                    if (cap != 3) {
+                        color[cap] = hashVal;
+                        cap++;
+                    }else{
+                        flag = false;
+                    }
+                }else {
+                    if (i != 3) {
+                        color2[i] = hashVal;
+                        i++;
+                    }else{
+                        flag = true;
+                    }
+                }
 			}
 		}
 		String res = "";
@@ -158,7 +206,59 @@ public class TestMain /*extends Application*/{
 			}
 		}
 		System.out.println(res);
+		String mess = "";
+        for(int row2 = 0; row2 < pic.length; row2++){
+            for(int col2 = 2; col2 < pic[row2].length; col2++){
+                String hexColor = pic[row2][col2].toString().substring(2,8);
+                String[] split = {hexColor.substring(0,2), hexColor.substring(2,4),hexColor.substring(4)};
+                for(String a : split){
+                    mess += hexToChar(a);
+                }
+            }
+        }
+        System.out.println(mess);
 	}
+
+	private char hexToChar(String g){
+	    return (char) hexToInt(g);
+    }
+
+    private int hexToInt(String twoDigitHex){
+        HashMap<Character, Integer> map = build();
+        char c1 = twoDigitHex.charAt(0);
+        char c2 = twoDigitHex.charAt(1);
+        int sum = 0;
+        for(Character a : map.keySet()){
+            if (c1 == a){
+                sum += map.get(a) * 16;
+            }
+            if(c2 == a){
+                sum += map.get(a);
+            }
+        }
+        return sum;
+    }
+
+    private HashMap<Character, Integer> build(){
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put(new Character('0'), new Integer(0));
+        map.put(new Character('1'), new Integer(1));
+        map.put(new Character('2'), new Integer(2));
+        map.put(new Character('3'), new Integer(3));
+        map.put(new Character('4'), new Integer(4));
+        map.put(new Character('5'), new Integer(5));
+        map.put(new Character('6'), new Integer(6));
+        map.put(new Character('7'), new Integer(7));
+        map.put(new Character('8'), new Integer(8));
+        map.put(new Character('9'), new Integer(9));
+        map.put(new Character('a'), new Integer(10));
+        map.put(new Character('b'), new Integer(11));
+        map.put(new Character('c'), new Integer(12));
+        map.put(new Character('d'), new Integer(13));
+        map.put(new Character('e'), new Integer(14));
+        map.put(new Character('f'), new Integer(15));
+        return map;
+    }
 
 
 
